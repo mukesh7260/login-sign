f42895f6cc2a002aecea0b7df03c972dbff64f5b
mukesh7260 <mk2648054@gmail.com>
2023-12-28 12:25:41 +0530

    save the file

---
+++ registration/app1/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
+from django.http import JsonResponse
+
+
+# Create your views here.
+@login_required(login_url='login')
+def HomePage(request):
+    return render (request,'home.html')
+
+def SignupPage(request):
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+
+        if pass1!=pass2:
+            return HttpResponse("Your password and confrom password are not Same!!")
+        else:
+
+            my_user=User.objects.create_user(uname,email,pass1)
+            my_user.save()
+            subject = 'Mail sent from mukesh kumar '
+            message = 'This is assignment task email '
+            from_email = 'mk2648054@gmail.com'
+            to_email = [email]
+            # try:
+            send_mail(subject, message, from_email, to_email)
+                # return JsonResponse({'message': 'Email sent successfully'})
+            # except Exception as e:
+            #     return JsonResponse({'message': str(e)}, status=500)
+            return redirect('login')
+                
+
+
+
+    return render (request,'signup.html')
+
+def LoginPage(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pass1=request.POST.get('pass')
+        user=authenticate(request,username=username,password=pass1)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return HttpResponse ("Username or Password is incorrect!!!")
+
+    return render (request,'login.html')
+
+def LogoutPage(request):
+    logout(request)
+    return redirect('login')
